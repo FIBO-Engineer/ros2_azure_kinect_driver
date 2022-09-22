89839553058410b2b68c2f74822b8549cb32df73
ckennedy2050 <ckennedy2050@gmail.com>
2022-09-22 11:39:48 -0700

    Build file repo cleanup

--- cmake-build-debug/ament_cmake_python/azure_kinect_ROS2_driver/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='azure_kinect_ROS2_driver',
-    version='0.0.1',
-    packages=find_packages(
-        include=('azure_kinect_ROS2_driver', 'azure_kinect_ROS2_driver.*')),
-)
