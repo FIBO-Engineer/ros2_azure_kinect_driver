e7a4f57723306288b10d8fc85b22494995aed995
ckennedy2050 <ckennedy2050@gmail.com>
2022-09-22 20:12:12 -0700

    Simplify launch files

--- launch/k4a_device_launch.py
+++ launch/k4a_device_launch.py
@@ -13,29 +13,7 @@ def generate_launch_description():
         emulate_tty=True
     )
 
-    ip_rgb_node = Node(
-        package="image_proc",
-        executable="image_proc",
-        remappings=[
-            ('image', '/k4a/rgb_to_depth/image_raw'),
-            ('image_rect', '/k4a/rgb_to_depth/image_rect'),
-            ('camera_info', '/k4a/rgb_to_depth/camera_info'),
-        ]
-    )
-
-    ip_depth_node = Node(
-        package="image_proc",
-        executable="image_proc",
-        remappings=[
-            ('image', '/k4a/depth/image_raw'),
-            ('image_rect', '/k4a/depth/image_rect'),
-            ('camera_info', '/k4a/depth/camera_info'),
-        ]
-    )
-
 
 
     ld.add_action(k4a_node)
-    ld.add_action(ip_rgb_node)
-    ld.add_action(ip_depth_node)
     return ld
--- launch/k4a_test_record_launch.py
+++ launch/k4a_test_record_launch.py
@@ -15,26 +15,6 @@ def generate_launch_description():
         ]
     )
 
-    ip_rgb_node = Node(
-        package="image_proc",
-        executable="image_proc",
-        remappings=[
-            ('image', '/k4a/rgb_to_depth/image_raw'),
-            ('image_rect', '/k4a/rgb_to_depth/image_rect'),
-            ('camera_info', '/k4a/rgb_to_depth/camera_info'),
-        ]
-    )
-
-    ip_depth_node = Node(
-        package="image_proc",
-        executable="image_proc",
-        remappings=[
-            ('image', '/k4a/depth/image_raw'),
-            ('image_rect', '/k4a/depth/image_rect'),
-            ('camera_info', '/k4a/depth/camera_info'),
-        ]
-    )
-
     k4a_test_node = Node(
         package="azure_kinect_ros2_driver",
         executable="test_azure_kinect_subscriber.py",
@@ -53,8 +33,6 @@ def generate_launch_description():
 
 
     ld.add_action(k4a_node)
-    ld.add_action(ip_rgb_node)
-    ld.add_action(ip_depth_node)
     ld.add_action(k4a_test_node)
     ld.add_action(req_exit)
 
